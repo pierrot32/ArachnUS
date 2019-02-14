b94f8a84095b4f332329322fb76f8335e9627129
Pierre-Olivier Lagacé <46456942+pierrot32@users.noreply.github.com>
2019-02-14 09:56:07 -0500

    Création projet interdace

---
+++ DocumentationElectronique/testMouvementPatteArduino/testServo/testServo.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+
+int x = 0;
+String reponsePycharm = "start";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Test mouvement des servos");
+
+}
+
+void loop() {
+
+
+  
+}
