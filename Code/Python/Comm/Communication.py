538bff5f02fab83b0053c06eaf8f69bb97651dc0
Pierre-Olivier Lagacé <46456942+pierrot32@users.noreply.github.com>
2019-02-14 10:55:41 -0500

    update pythonComm 0.4

---
+++ Code/Python/Comm/Communication.py
@@ -0,0 +1,29 @@
+import serial
+
+#arduino = serial.Serial('COM3', 9600)
+
+
+def manuel(moteur1, moteur2):
+    print("manuel")
+    #arduino.write(b'manuel')
+    #arduino.write(moteur1)
+    #arduino.write(moteur2)
+
+def stop():
+    print("stop")
+    #arduino.write(b'stop')
+
+def initalisation():
+    print("init")
+    #arduino.write(b'init')
+
+def lectureArduino():
+    print("lA")
+    #entete = arduino.readline()
+    #if entete==
+
+    #confirmationEtat = arduino.readline()
+    #commandeMoteur0 = arduino.readline()
+
+
+#arduino.write(b'stop')
