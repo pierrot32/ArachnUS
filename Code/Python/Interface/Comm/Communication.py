7f5556d2d0179b756035372ca92c9d762f444a17
Pierre-Olivier Lagacé <46456942+pierrot32@users.noreply.github.com>
2019-02-14 14:51:52 -0500

    update pythonComm 0.7

--- Code/Python/Interface/Comm/Communication.py
+++ Code/Python/Interface/Comm/Communication.py
@@ -1,6 +1,14 @@
 import serial
+import struct
+import glob
+import struct
+import time
+import numpy as np
 
 #arduino = serial.Serial('COM3', 9600)
+
+moteur = [0,0,0,0,0,0,0]
+etat = 0
 
 
 def manuel(moteur1, moteur2):
@@ -26,5 +34,45 @@ def lectureArduino():
     #confirmationEtat = arduino.readline()
     #commandeMoteur0 = arduino.readline()
 
+#-----------------------------------------------------------------------------------------
 
-#arduino.write(b'stop')
+
+
+
+
+
+
+class ReadFromArduino(object):
+    """A class to read the serial messages from Arduino. The code running on Arduino
+    can for example be the ArduinoSide_LSM9DS0 sketch."""
+
+    def __init__(self, port, SIZE_STRUCT=12, verbose=0):
+        self.port = port
+        self.SIZE_STRUCT = SIZE_STRUCT
+        self.verbose = verbose
+        self.latest_values = -1
+        self.t = 0
+        self.port.flushInput()
+
+    def read_one_value(self):
+        """Wait for next serial message from the Arduino, and read the whole
+        message as a structure."""
+        read = False
+
+        while not read:
+            data = self.port.read(self.SIZE_STRUCT)
+            print(data)
+            new_values = struct.unpack('iii', data)
+            print(new_values)
+
+    def envoieVersArduinoEtat1(self,moteur):
+        struct.pack('iii', etat, moteur(0), moteur(1))
+
+
+
+
+
+arduino = serial.Serial('COM3', 9600)
+
+Arduino = ReadFromArduino(arduino)
+Arduino.read_one_value()
