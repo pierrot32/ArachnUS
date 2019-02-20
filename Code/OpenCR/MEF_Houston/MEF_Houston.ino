87982f368dcce3b946ae4af00441cd2f0171fa46
WilliamDubois <46566983+WilliamDubois@users.noreply.github.com>
2019-02-20 17:08:58 -0500

    update MEF_Houston 1.2

--- Code/OpenCR/MEF_Houston/MEF_Houston.ino
+++ Code/OpenCR/MEF_Houston/MEF_Houston.ino
@@ -1,5 +1,8 @@
+
+#define NBR_DE_SERVO 8
 // variables globales
 int initDone = 0;
+
 typedef struct message{
   long etat;
   long moteur0;
@@ -17,17 +20,23 @@ typedef struct message{
 };
 message MSG_Envoi;
 message MSG_Recu;
-int long_msg_envoi = sizeof(MSG_Envoi);
-int long_msg_recu = sizeof(MSG_Recu);
 byte buff[sizeof(MSG_Recu)];
+int valeurs_Angles_moteurs[NBR_DE_SERVO];
 
 void setup() {
 Serial.begin(115200);
+initFctServo(valeurs_Angles_moteurs);
 }
 
 void loop() {
   while(Serial.available() > 0){
   }
+
+  //lis les données envoyées par python
+  bool x;
+  x = lecture(buff);
+  copie_array_struc(&MSG_Recu, buff,valeurs_Angles_moteurs);
+  
   switch(MSG_Recu.etat){
     case 0:
       MSG_Envoi.etat = 0;
@@ -36,21 +45,21 @@ void loop() {
       break;
     case 1:
       MSG_Envoi.etat = 1;
-      //etat à venir
+      //etat init
       initDone = 1;
       break;
     case 2:
-      //etat à venir
-      if (initDone == 0){
-        MSG_Envoi.etat = 0;
-        break;
-        //init n'a pas été fait donc on ne peut entrer en mode manuel
-      }
-      else{
+      //etat manuel verifie si le robot a ete initialise, mais n'est pas encore necessaire puisque nous n'avons pas d'initialisation a faire pour l'instant
+//      if (initDone == 0){
+//        MSG_Envoi.etat = 0;
+//        break;
+//        //init n'a pas été fait donc on ne peut entrer en mode manuel
+//      }
+//      else{
         MSG_Envoi.etat = 2;
-        //opération demandée
+        updateServos(valeurs_Angles_moteurs);
         break;        
-      }
+//        }
 
 /*    case 3:
       //etat à venir
@@ -65,24 +74,34 @@ void loop() {
 
       break;*/
   }
-//  bool x;
-//  x = lecture(buff);
-//  copie_array_struc(&MSG_Recu, buff);
-  
-  
-  delay(2000);
+  envoi_serie(MSG_Envoi, valeurs_Angles_moteurs);
+  delay(100);
 }
-//fonction de lecture
+
+//fonction de lecture des données envoyées du PC à la carte (stocke toutes les données dans un buffer)
 bool lecture(byte *buf){
   return(Serial.readBytes((char*)buf, sizeof(message))==sizeof(message)); 
 }
 
-//fonction de copie array dans struc
-void copie_array_struc(message *MSG, byte *buf){
+//fonction de copie d'un byte array dans une struc et création du tableau de valeurs des angles des moteurs à partir du byte array
+void copie_array_struc(message *MSG, byte *buf, int valeurs_angles[]){
+  long tableau[NBR_DE_SERVO];
   memcpy(&MSG, buf, sizeof(MSG));
+  memcpy(tableau, buf+=sizeof(long), sizeof(tableau));
+  for (int i=0; i<NBR_DE_SERVO;i++){
+    valeurs_angles[i] = (int)tableau[i];
+  }
 }
 
-//fontion d'envoi
-void envoi_serie(message MSG){
-  Serial.write((uint8_t*)&MSG_Recu, long_msg_recu);
+//fontion d'envoi des données de la carte openCR au PC
+void envoi_serie(message MSG, int valeurs_angles[]){
+  MSG.moteur0 = valeurs_angles[0];
+  MSG.moteur1 = valeurs_angles[1];
+  MSG.moteur2 = valeurs_angles[2];
+  MSG.moteur3 = valeurs_angles[3];
+  MSG.moteur4 = valeurs_angles[4];
+  MSG.moteur5 = valeurs_angles[5];
+  MSG.moteur6 = valeurs_angles[6];
+  MSG.moteur7 = valeurs_angles[7];
+  Serial.write((uint8_t*)&MSG, sizeof(MSG));
 }
---
+++ Code/OpenCR/MEF_Houston/fonctionsServos.ino
@@ -0,0 +1,56 @@
+/*************************************************** 
+  Actualisation de l'ensemble des servos
+ ****************************************************/
+ 
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// L'adresse de la carte PCA9685 est 0x41.
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+
+//À valider:
+#define SERVOMIN  250 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  450 // this is the 'maximum' pulse length count (out of 4096)
+#define ANGLEMIN  0
+#define ANGlEMAX  90
+#define NBR_DE_SERVO 8
+
+// Convertie une consigne d'angle au moteur en une longueur de pulse pour le pwm
+int angleAPulse(int angle) {
+  if(angle<ANGLEMIN){
+    angle = ANGLEMIN;
+  } else if (angle>ANGlEMAX) {
+    angle = ANGlEMAX;
+  }
+  return map(angle, ANGLEMIN, ANGlEMAX, SERVOMIN, SERVOMAX);
+}
+
+// Convertie une consigne de pulse au moteur en une valeur d'angle
+int pulseAAngle(int pulse) {
+  if(pulse<SERVOMIN){
+    pulse = SERVOMIN;
+  } else if (pulse>SERVOMAX){
+    pulse = SERVOMAX;
+  }
+  return map(pulse, SERVOMIN, SERVOMAX, ANGLEMIN, ANGlEMAX);
+}
+
+// Actualise l'ensemble des servos un après l'autre
+void updateServos(int angleVoulu[NBR_DE_SERVO]) {
+  for (int i=0; i<NBR_DE_SERVO; i++){
+    pwm.setPWM(i, 0, angleAPulse(angleVoulu[i])); 
+  }
+}
+
+void initFctServo(int valeursAngle[]) {
+
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Frequence de raffraichissement des servo est ~60 Hz
+
+  int valeurMoy = ((SERVOMAX-SERVOMIN)/2)+SERVOMIN;
+
+  for (int i=0; i<NBR_DE_SERVO; i++){
+    valeursAngle[i] = pulseAAngle(valeurMoy); 
+  }
+  updateServos(valeursAngle);
+}
