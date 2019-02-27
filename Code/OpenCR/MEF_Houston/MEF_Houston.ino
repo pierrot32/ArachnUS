fc543141bb1c3a4fb549f8d598be774faa1e779a
WilliamDubois <46566983+WilliamDubois@users.noreply.github.com>
2019-02-27 10:18:31 -0500

    update MEF_houston 2.0

--- Code/OpenCR/MEF_Houston/MEF_Houston.ino
+++ Code/OpenCR/MEF_Houston/MEF_Houston.ino
@@ -1,21 +1,9 @@
 
 #define NBR_DE_SERVO 8
 // variables globales
+
 int initDone = 0;
 
-typedef struct recoit{
-//  long debut_struct;
-  long etat;
-  long moteur0;
-  long moteur1;
-  long moteur2;
-  long moteur3;
-  long moteur4;
-  long moteur5;
-  long moteur6;
-  long moteur7;
-  long hauteur;
-};
 typedef struct envoi{
 //  long debut_struct;
   long etat;
@@ -34,10 +22,10 @@ typedef struct envoi{
 };
 
 envoi msg_Envoi;
-recoit msg_Recu;
+
 int height[NBR_DE_SERVO];
 int state[NBR_DE_SERVO];
-byte buff[sizeof(msg_Recu)];
+byte buff[10*sizeof(long)];
 int valeurs_Angles_moteurs[NBR_DE_SERVO];
 
 void setup() {
@@ -49,18 +37,17 @@ void loop() {
   while(Serial.available() > 0){
     bool x;
     x = lecture(buff);
-    copie_array_struc(&msg_Recu, buff,valeurs_Angles_moteurs, state, height);
+    copie_array_struc(buff,valeurs_Angles_moteurs, state, height);
   }
-//  Serial.println("\nmoteur0");
-//  Serial.println(valeurs_Angles_moteurs[0]);
+
   switch(state[0]){
     case 0:
       initDone = 0;
       //etat arret fait rien
       break;
     case 1:
-//      msg_Envoi.etat = 1;
-      //etat init
+      //état d'initialisation
+      msg_Envoi.etat = 1;
       initDone = 1;
       break;
     case 2:
@@ -71,7 +58,7 @@ void loop() {
 //        //init n'a pas été fait donc on ne peut entrer en mode manuel
 //      }
 //      else{
-//        msg_Envoi.etat = 2;
+        msg_Envoi.etat = 2;
         updateServos(valeurs_Angles_moteurs);
         break;        
 //        }
@@ -90,50 +77,6 @@ void loop() {
       break;*/
   }
   envoi_serie(msg_Envoi,valeurs_Angles_moteurs);
-//  Serial.println("envoi");
-//  Serial.println(MSG_Envoi.moteur0);
-//  Serial.println("envoi");
-//  Serial.println(MSG_Envoi.etat);
-//  Serial.println(MSG_Envoi.moteur0);
-//  Serial.println(MSG_Envoi.moteur1);
-//  Serial.println(MSG_Envoi.moteur2);
-//  Serial.println(MSG_Envoi.moteur3);
-//  Serial.println(MSG_Envoi.moteur4);
-//  Serial.println(MSG_Envoi.moteur5);
-//  Serial.println(MSG_Envoi.moteur6);
-//  Serial.println(MSG_Envoi.moteur7);
+
   delay(100);
-}
-
-//fonction de lecture des données envoyées du PC à la carte (stocke toutes les données dans un buffer)
-bool lecture(byte *buf){
-  return(Serial.readBytes((char*)buf, sizeof(recoit))==sizeof(recoit)); 
-}
-
-//fonction de copie d'un byte array dans une struc et création du tableau de valeurs des angles des moteurs à partir du byte array
-void copie_array_struc(recoit *MSG, byte *buf, int valeurs_angles[], int state[], int hauteur[]){
-  long tableau[NBR_DE_SERVO+2];
-  memcpy(&MSG, buf, sizeof(MSG));
-//  memcpy(&state, buf+=sizeof(long), sizeof(long));
-  memcpy(tableau, buf, sizeof(tableau));
-  state[0] = (int)tableau[0];
-  for (int i=0; i<NBR_DE_SERVO;i++){
-    valeurs_angles[i] = (int)tableau[i+1];
-  }
-  hauteur[0] = (int)tableau[NBR_DE_SERVO+1];
-}
-
-//fontion d'envoi des données de la carte openCR au PC
-void envoi_serie(envoi MSG, int valeurs_moteurs[NBR_DE_SERVO]){
-  msg_Envoi.etat = state[0];
-  msg_Envoi.hauteur = height[0];
-  MSG.moteur0 = valeurs_moteurs[0];
-  MSG.moteur1 = valeurs_moteurs[1];
-  MSG.moteur2 = valeurs_moteurs[2];
-  MSG.moteur3 = valeurs_moteurs[3];
-  MSG.moteur4 = valeurs_moteurs[4];
-  MSG.moteur5 = valeurs_moteurs[5];
-  MSG.moteur6 = valeurs_moteurs[6];
-  MSG.moteur7 = valeurs_moteurs[7];
-  Serial.write((uint8_t*)&MSG, sizeof(MSG));
 }
---
+++ Code/OpenCR/MEF_Houston/fonctionsComm.ino
@@ -0,0 +1,30 @@
+//fonction de lecture des données envoyées du PC à la carte (stocke toutes les données dans un buffer)
+bool lecture(byte *buf){
+  return(Serial.readBytes((char*)buf, 10*sizeof(long))== 10*sizeof(long)); 
+}
+
+//fonction de copie d'un byte array dans une struc et création du tableau de valeurs des angles des moteurs à partir du byte array
+void copie_array_struc(byte *buf, int valeurs_angles[], int state[], int hauteur[]){
+  long tableau[NBR_DE_SERVO+2];
+  memcpy(tableau, buf, sizeof(tableau));
+  state[0] = (int)tableau[0];
+  for (int i=0; i<NBR_DE_SERVO;i++){
+    valeurs_angles[i] = (int)tableau[i+1];
+  }
+  hauteur[0] = (int)tableau[NBR_DE_SERVO+1];
+}
+
+//fontion d'envoi des données de la carte openCR au PC
+void envoi_serie(envoi MSG, int valeurs_moteurs[NBR_DE_SERVO]){
+  MSG.etat = state[0];
+  MSG.hauteur = height[0];
+  MSG.moteur0 = valeurs_moteurs[0];
+  MSG.moteur1 = valeurs_moteurs[1];
+  MSG.moteur2 = valeurs_moteurs[2];
+  MSG.moteur3 = valeurs_moteurs[3];
+  MSG.moteur4 = valeurs_moteurs[4];
+  MSG.moteur5 = valeurs_moteurs[5];
+  MSG.moteur6 = valeurs_moteurs[6];
+  MSG.moteur7 = valeurs_moteurs[7];
+  Serial.write((uint8_t*)&MSG, sizeof(MSG));
+}
