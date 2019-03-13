3fd6b8ceef225d5586159cc9b3e3804fd8c1bd83
SamuelFaucher <46575550+SamuelFaucher@users.noreply.github.com>
2019-03-13 15:43:29 -0400

    Première version des codes pour faire et tester la cinématique

---
+++ Code/OpenCR/ControleDunBras/testCalculCinema/testCalculCinema.ino
@@ -0,0 +1,26 @@
+/*************************************************** 
+  Tests pour valider les fonctionnalitées du notre code pour la cinématique des pattes
+ ****************************************************/
+
+
+#include "matrix.h"
+
+
+matrix_obj *matriceTest1;
+float index = 0.0;
+
+void setup() {
+  matriceTest1 = matrix_construct_zero(3, 3);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  matrix_printf(matriceTest1);
+  Serial.println();
+  delay(500);
+
+  index += 0.1;
+  matriceTest1->array[1][1] = index;
+
+}
